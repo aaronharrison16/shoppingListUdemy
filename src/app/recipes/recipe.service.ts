53795e6d681f78b77c6bcd6ed9f6eb04a6647a80
Aaron Harrison <aaronharrison16@gmail.com>
2019-06-26 15:20:39 -0600

    fixed bug that threw an error when the page was refreshed when on a index dependant route using a resolver service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,42 +7,16 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+    private recipes: Recipe[] = [];
+
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe',
-            'This is simply a test',
-            'https://pinchofyum.com/wp-content/uploads/Lo-Mein-1-2-768x1152.jpg',
-            [
-                new Ingredient('meat', 1),
-                new Ingredient('cheese', 6),
-                new Ingredient('bread', 84),
-                new Ingredient('carrots', 11)
-            ]),
-        new Recipe(
-            'Another Test Recipe', 
-            'This is also a test thank you', 
-            'https://www.twopeasandtheirpod.com/wp-content/uploads/2017/03/Thai-Cabbage-Salad-1.jpg',
-            [
-                new Ingredient('beans', 1),
-                new Ingredient('chocolate', 8),
-                new Ingredient('french fries', 7),
-                new Ingredient('deer', 9)
-            ]),
-        new Recipe(
-            'And Another Test Recipe',
-            'ANOTHER TEST BUT IN CAPS', 
-            'https://www.thedeliciouscrescent.com/wp-content/uploads/2016/06/Easy-Hyderabadi-Chicken-Biryani-4.jpg',
-            [
-                new Ingredient('pork', 1),
-                new Ingredient('watermelon', 51),
-                new Ingredient('gravy', 8),
-                new Ingredient('noodles', 81)
-            ]),
-    ]
-
     constructor(private slService: ShoppingListService) {}
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
